3389ff997a28458723fdc107dd1bfcb1fa8ce494
Harshal-creator <harshalshelar8251@gmail.com>
2024-01-02 22:45:06 +0530

    header added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TechSkillsComponent } from './components/tech-skills/tech-skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { WorkComponent } from './components/work/work.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { OtherSkillsComponent } from './components/other-skills/other-skills.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", redirectTo : 'profile', pathMatch : 'full'},
+  {path : "profile", component : ProfileComponent},
+  {path : "techSkills", component : TechSkillsComponent},
+  {path : "education", component : EducationComponent},
+  {path : "work", component : WorkComponent},
+  {path : "projects", component : ProjectsComponent},
+  {path : "resume", component : ResumeComponent},
+  {path : "otherSkills", component : OtherSkillsComponent},
+  {path : "contactMe", component : ContactMeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
