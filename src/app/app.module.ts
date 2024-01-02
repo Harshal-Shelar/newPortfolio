bd94548b017a3fec449f1b56f5b3240c87069622
Harshal-creator <harshalshelar8251@gmail.com>
2024-01-02 21:57:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { WorkComponent } from './components/work/work.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TechSkillsComponent } from './components/tech-skills/tech-skills.component';
+import { OtherSkillsComponent } from './components/other-skills/other-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProjectsComponent,
+    WorkComponent,
+    EducationComponent,
+    ContactMeComponent,
+    ResumeComponent,
+    HeaderComponent,
+    TechSkillsComponent,
+    OtherSkillsComponent
   ],
   imports: [
     BrowserModule,
